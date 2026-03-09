b3a5d201fe257cca38fcebd3342cf2f29a76bb66
hhj <gkagus98@naver.com>
2026-03-09 15:49:03 +0900

    SUBWAY

---
+++ js/productData.js
@@ -0,0 +1,257 @@
+export const productData = {
+    "tab-0": [ // 블론드 로스트
+        {
+            "classification": "클래식",
+            "price": "5,900",
+            "name": "참치",
+            "image": "images/tab-1-1.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "클래식",
+            "price": "7,000",
+            "name": "햄",
+            "image": "images/tab-1-2.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "클래식",
+            "price": "6,000",
+            "name": "에그마요",
+            "image": "images/tab-1-2.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "클래식",
+            "price": "7,000",
+            "name": "비엘티",
+            "image": "images/tab-1-4.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "클래식",
+            "price": "9,500",
+            "name": "이탈리안 비엠티",
+            "image": "images/tab-1-5.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "클래식",
+            "price": "5,900",
+            "name": "참치",
+            "image": "images/tab-1-1.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "클래식",
+            "price": "7,000",
+            "name": "햄",
+            "image": "images/tab-1-2.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "클래식",
+            "price": "6,000",
+            "name": "에그마요",
+            "image": "images/tab-1-2.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "클래식",
+            "price": "7,000",
+            "name": "비엘티",
+            "image": "images/tab-1-4.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "클래식",
+            "price": "9,500",
+            "name": "이탈리안 비엠티",
+            "image": "images/tab-1-5.png",
+            "profile_link": "#"
+        },
+    ],
+    "tab-1": [ // 프레쉬 & 라이트
+        {
+            "classification": "프레쉬 & 라이트",
+            "price": "6,400",
+            "name": "New 머쉬룸",
+            "image": "images/tab-2-1.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "프레쉬 & 라이트",
+            "price": "7,800",
+            "name": "로스트 치킨",
+            "image": "images/tab-2-2.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "프레쉬 & 라이트",
+            "price": "7,800",
+            "name": "로티세리 바비큐 치킨",
+            "image": "images/tab-2-3.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "프레쉬 & 라이트",
+            "price": "5,100",
+            "name": "베지",
+            "image": "images/tab-2-4.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "프레쉬 & 라이트",
+            "price": "5,400",
+            "name": "에그 슬라이스 ",
+            "image": "images/tab-2-5.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "프레쉬 & 라이트",
+            "price": "8,700",
+            "name": "터키 베이컨 아보카도",
+            "image": "images/tab-2-6.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "프레쉬 & 라이트",
+            "price": "6,900",
+            "name": "터키",
+            "image": "images/tab-2-7.png",
+            "profile_link": "#"
+        },
+
+    ],
+    "tab-2": [ // 샐러드
+        {
+            "classification": "샐러드",
+            "price": "13,000",
+            "name": "풀드포크 타코 샐러드",
+            "image": "images/tab-3-1.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "샐러드",
+            "price": "13,200",
+            "name": "스파이시 쉬림프 타코 샐러드",
+            "image": "images/tab-3-2.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "샐러드",
+            "price": "12,500",
+            "name": "로티세리 치킨 타코 샐러드",
+            "image": "images/tab-3-3.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "샐러드",
+            "price": "11,500",
+            "name": "안창 비프",
+            "image": "images/tab-3-4.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "샐러드",
+            "price": "12,500",
+            "name": "안창 비프 & New 머쉬룸",
+            "image": "images/tab-3-5.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "샐러드",
+            "price": "11,500",
+            "name": "New 머쉬룸",
+            "image": "images/tab-3-6.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "샐러드",
+            "price": "13,200",
+            "name": "이탈리안 비엠티 ",
+            "image": "images/tab-3-7.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "샐러드",
+            "price": "11,900",
+            "name": "풀드포크 바비큐 ",
+            "image": "images/tab-3-8.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "샐러드",
+            "price": "8,200",
+            "name": "터키 베이컨 아보카도",
+            "image": "images/tab-3-9.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "샐러드",
+            "price": "7,800",
+            "name": "써브웨이 클럽",
+            "image": "images/tab-3-10.png",
+            "profile_link": "#"
+        },
+        
+    ],
+    "tab-3": [ // 아침메뉴 & 스마일 썹
+        {
+            "classification": "아침메뉴",
+            "price": "2,900",
+            "name": "햄, 에그&치즈",
+            "image": "images/tab-4-1.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "아침메뉴",
+            "price": "2,900",
+            "name": "웨스턴, 에그& 치즈",
+            "image": "images/tab-4-2.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "아침메뉴",
+            "price": "2,900",
+            "name": "햄, 에그&치즈 랩",
+            "image": "images/tab-4-3.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "아침메뉴",
+            "price": "2,900",
+            "name": "웨스턴, 에그&치즈 랩",
+            "image": "images/tab-4-4.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "스마일썹",
+            "price": "3400",
+            "name": "치킨 베이컨 미니 랩",
+            "image": "images/tab-4-5.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "스마일썹",
+            "price": "3,200",
+            "name": "포테이토 베이컨 수프",
+            "image": "images/tab-4-6.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "스마일썹",
+            "price": "3,200",
+            "name": "콘 수프",
+            "image": "images/tab-4-7.png",
+            "profile_link": "#"
+        },
+        {
+            "classification": "스마일썹",
+            "price": "3,200",
+            "name": "머쉬룸 수프",
+            "image": "images/tab-4-8.png",
+            "profile_link": "#"
+        },
+    ]
+};
